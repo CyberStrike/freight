f44917116455500763bd4cff45ae7cae613948bc
Chris Scott <Chris@Gnmlbs.com>
2014-12-22 04:16:45 -0500

    added faker to Post?Factory

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :post do
     user nil
-    title "MyString"
-    body "MyText"
+    title Faker::Lorem.characters(character_count = 30)
+    body Faker::DizzleIpsum.paragraphs(paragraph_count = 3).join(',')
     published false
   end
 end
