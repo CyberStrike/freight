067bde227123c71943ce364155d03b79df57123e
Chris Scott <Chris@Gnmlbs.com>
2014-12-22 00:45:13 -0500

    configured rspec to use factory girl namespace

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 require 'capybara/rails'
@@ -27,6 +28,10 @@ require 'capybara/rails'
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+
+  # Stop factory_girl methods needing to be prefaced with FactoryGirl.
+  config.include FactoryGirl::Syntax::Methods
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
