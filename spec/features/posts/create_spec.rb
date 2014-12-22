beac61218c53edf605177c7ab86780f664d57732
Chris Scott <Chris@Gnmlbs.com>
2014-12-22 03:53:04 -0500

    fixed typos in post?create_spec

--- spec/features/posts/create_spec.rb
+++ spec/features/posts/create_spec.rb
@@ -6,19 +6,19 @@ context 'When creating a Post', :type => :feature do
 
   before :each do
     visit '/posts'
-    click_on 'Add Post'
+    click_on 'New Post'
   end
 
   def post_defaults
     expect(page).to have_content 'New post'
     fill_in 'Title', with: post.title
-    fill_in 'Description', with: post.description
+    fill_in 'Body', with: post.body
   end
 
   it 'it saves successfully' do
     post_defaults
     click_on 'Save'
-    expect(page).to have_content post.description
+    expect(page).to have_content post.body
   end
 
 end
